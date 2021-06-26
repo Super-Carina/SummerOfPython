0bcd87d9fd452fcd689975a54e4e31cc2f3c15fb
carina <carina.jong.mena@gmail.com>
2021-06-26 15:32:07 -0700

    remove cruft

--- listnumbertwo.py
+++ listnumbertwo.py
@@ -4,20 +4,6 @@ mouse = ["black", "blue", "purple", "red"]
 
 something = "that"
 
-print(tophat)
-
-# tophat.sort()
-
-# tophat.reverse()
-
-print(tophat.count("pizza"))
-
-
-# print("tophat:", len(tophat))
-# print("mouse:", len(mouse))
-# print("something:", len("that"))
-
-print(tophat[2:5])
 
 # homework 1
 # print the last element of tophat. 
