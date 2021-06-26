6d9b7a52f768a6da6f19ddc961ff6309bf412b7f
carina <carina.jong.mena@gmail.com>
2021-06-26 15:30:12 -0700

    More lists.

--- listnumbertwo.py
+++ listnumbertwo.py
@@ -4,3 +4,22 @@ mouse = ["black", "blue", "purple", "red"]
 
 something = "that"
 
+print(tophat)
+
+# tophat.sort()
+
+# tophat.reverse()
+
+print(tophat.count("pizza"))
+
+
+# print("tophat:", len(tophat))
+# print("mouse:", len(mouse))
+# print("something:", len("that"))
+
+print(tophat[2:5])
+
+# homework 1
+# print the last element of tophat. 
+# hint: use len
+
