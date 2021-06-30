06149922b0e19dfcb3245bd7c723a8b6d11e83d9
carina <carina.jong.mena@gmail.com>
2021-06-30 09:41:37 -0700

    Some other Python projects.

--- listnumbertwo.py
+++ listnumbertwo.py
@@ -1,11 +1,17 @@
 tophat = ["chocolate", "rice", "pizza", "macandcheese", "popcorn", "seeweed", "apples", "pizza", "zucchini",]
 
+tophat2 = []
+
 mouse = ["black", "blue", "purple", "red"]
 
 something = "that"
 
 
 # homework 1
-# print the last element of tophat. 
-# hint: use len
+# print the last element of tophat using only len.
+# test it with an empty list []
+#
+#
+
+
 
---
+++ planets.py
@@ -0,0 +1,6 @@
+def mercury(weight_on_earth):
+    return weight_on_earth * 0.1;
+
+def earth(weight_on_earth):
+    return weight_on_earth * 1;
+
